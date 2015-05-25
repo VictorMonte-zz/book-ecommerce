03eec71ea52b7224fda70845add1bad0dbc4c02b
Jonatan Cardoso Saraiva <jonatan.cardoso@hotmail.com.br>
2015-05-25 00:52:19 -0300

    Author Bean
    Author Bean

--- src/br/com/estore/web/model/AuthorBean.java
+++ src/br/com/estore/web/model/AuthorBean.java
@@ -4,86 +4,79 @@ import java.util.Date;
 
 public class AuthorBean {
 
-	private Long id;
-	private String nome;
-	private String telefone;
-	private String cpf;
-	private GenderBean gender;
+	private int id;
+	private String name;
+	private String lastName;
 	private String email;
+	private String phone;
+	private String nacionality;
 	private Date birthDate;
-	private NacionalityBean nacionality;
-	private String description;
-
-	public Long getId() {
+	private String gender;
+	private String address;	
+	private String spouse;
+	
+	
+	public int getId() {
 		return id;
 	}
-
-	public void setId(Long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getNome() {
-		return nome;
+	public String getName() {
+		return name;
 	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
-
-	public String getTelefone() {
-		return telefone;
+	public String getLastName() {
+		return lastName;
 	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public GenderBean getGender() {
-		return gender;
-	}
-
-	public void setGender(GenderBean gender) {
-		this.gender = gender;
-	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getNacionality() {
+		return nacionality;
+	}
+	public void setNacionality(String nacionality) {
+		this.nacionality = nacionality;
+	}
 	public Date getBirthDate() {
 		return birthDate;
 	}
-
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
 
-	public NacionalityBean getNacionality() {
-		return nacionality;
+	public String getGender() {
+		return gender;
 	}
-
-	public void setNacionality(NacionalityBean nacionality) {
-		this.nacionality = nacionality;
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
-
-	public String getDescription() {
-		return description;
+	public String getAddress() {
+		return address;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
+	public void setAddress(String address) {
+		this.address = address;
 	}
+	public String getSpouse() {
+		return spouse;
+	}
+	public void setSpouse(String spouse) {
+		this.spouse = spouse;
+	}	
 
 }
